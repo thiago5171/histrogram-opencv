64946c7f9c38cd6c75bd752ce3c4f6ae00e0783b
thiago5171 <tgazaroli@gmail.com>
2022-11-03 21:26:16 -0300

    Do compare frame and image and print

--- main.py
+++ main.py
@@ -8,18 +8,42 @@
 # cv2.imshow('original', image)
 # # cv2.imshow('result', hsv)
 # cv2.waitKey()
+from __future__ import print_function
+from __future__ import division
 import cv2 as cv
+
+src_base = cv.imread('img.png')
+hsv_base = cv.cvtColor(src_base, cv.COLOR_BGR2HSV)
+h_bins = 50
+s_bins = 60
+histSize = [h_bins, s_bins]
+
+h_ranges = [0, 180]
+s_ranges = [0, 256]
+ranges = h_ranges + s_ranges  # concat lists
+channels = [0, 1]
+
+hist_base = cv.calcHist([hsv_base], channels, None, histSize, ranges, accumulate=False)
+cv.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+base_base = cv.compareHist(hist_base, hist_base, cv.HISTCMP_CORREL)
+
 
 vidcap = cv.VideoCapture('video.mp4')
 success, image = vidcap.read()
 count = 0
 while success:
+    hsv_frame = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    hist_frame = cv.calcHist([hsv_frame], channels, None, histSize, ranges, accumulate=False)
+    cv.normalize(hist_frame, hist_frame, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
+    comp1 = cv.compareHist(hist_base, hist_frame, cv.HISTCMP_CORREL)
+    comp2 = cv.compareHist(hist_base, hist_frame, cv.HISTCMP_INTERSECT)
+    print("comp1:", comp1, "comp2", comp2)
     success, image = vidcap.read()
     if count ==500 :
         a = image
-    # print('Read a new frame: ', success, count)
+
     count += 1
 
-cv.imshow('result', a)
-cv.waitKey()
+# cv.imshow('result', a)
+# cv.waitKey()
 
