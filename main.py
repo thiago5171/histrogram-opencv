7a2faec8d9eab70e71b4acd5c264c60d0a3559ec
Thiago Gazaroli Cardoso <Tgazaroli@gmail.com>
2022-11-04 16:51:13 -0300

    enhancement compare hist

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# import cv2
-#
-# image = cv2.imread('poke.jpg', cv.IMREAD_GRAYSCALE)
-# hist = cv2.calcHist([image], [0], None, [256], [0, 255])
-# plt.hist(image.ravel(), 256,  [0, 256])
-# plt.show()
-# # cv2.imshow('mask', realMask)
-# cv2.imshow('original', image)
-# # cv2.imshow('result', hsv)
-# cv2.waitKey()
 from __future__ import print_function
 from __future__ import division
 import cv2 as cv
@@ -23,6 +13,7 @@ cv.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
 vidcap = cv.VideoCapture('video.mp4')
 success, image = vidcap.read()
 count = 0
+exist_in_video = False
 while success:
     hsv_frame = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
@@ -34,10 +25,15 @@ while success:
     success, image = vidcap.read()
     if comp2 == 0.0 or comp1 == 1.0:
         start = cv.equalizeHist(hsv_base)
-        video_frame = cv.equalizeHist(hsv_base)
+        video_frame = cv.equalizeHist(hsv_frame)
+        exist_in_video = True
     count += 1
 
-cv.imshow('result', video_frame)
-cv.imshow('original', start)
+
+if exist_in_video:
+    cv.imshow('result', video_frame)
+    cv.imshow('original', start)
+else:
+    print("A imagem nao faz parte do video!")
 cv.waitKey()
 
