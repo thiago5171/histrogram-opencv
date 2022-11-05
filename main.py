039e3fc1a25161e2b4e8d96394e6e5a5fca5215e
thiago5171 <tgazaroli@gmail.com>
2022-11-04 21:13:24 -0300

    Do compare frame and image and print

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from __future__ import division
 import cv2 as cv
 
 
-comp_method = input("Qual metodo de comparação deseja utilizar \n[1] - HISTCMP_CORREL \n[2] - HISTCMP_CHISQR")
+comp_method = int(input("Qual metodo de comparação deseja utilizar \n[0] - HISTCMP_CORREL \n[1] - HISTCMP_CHISQR:  "))
 
+print(cv.HISTCMP_CHISQR)
+print(cv.HISTCMP_CORREL)
+
+if comp_method != 0 and comp_method != 1:
+    print("Opção inválida")
+    exit()
 
 src_base = cv.imread('img1.png')
 hsv_base = cv.cvtColor(src_base, cv.COLOR_BGR2GRAY)
@@ -23,14 +29,20 @@ while success:
 
     hist_frame = cv.calcHist([hsv_frame],  [0], None, [256], [0, 256])
     cv.normalize(hist_frame, hist_frame, alpha=0, beta=1, norm_type=cv.NORM_MINMAX)
-    comp1 = cv.compareHist(hist_base, hist_frame, cv.HISTCMP_CORREL)
-    comp2 = cv.compareHist(hist_base, hist_frame, cv.HISTCMP_CHISQR)
-    print("comp1:  ", comp1, " comp2 : ", comp2, "  cont", count)
-    success, image = vidcap.read()
-    if comp2 == 0.0 or comp1 == 1.0:
+    comp = cv.compareHist(hist_base, hist_frame, comp_method)
+
+    if -0.1 > comp < 0.3:
         start = cv.equalizeHist(hsv_base)
         video_frame = cv.equalizeHist(hsv_frame)
         exist_in_video = True
+        print(" comp2 : ", comp, "  cont", count)
+    if 0.89 < comp < 1.1:
+        start = cv.equalizeHist(hsv_base)
+        video_frame = cv.equalizeHist(hsv_frame)
+        exist_in_video = True
+        print(" comp2 : ", comp, "  cont", count)
+
+    success, image = vidcap.read()
     count += 1
 
 
